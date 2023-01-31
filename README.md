02f70854f0906e04e616e394128beb15662a6aa4
Stefan Maak <stefanmaak@freenet.de>
2023-01-31 23:55:07 +0100

    install-script does a lot of work :-)

--- README.md
+++ README.md
@@ -2,55 +2,6 @@
 
 
 
-## Getting started
-
-To make it easy for you to get started with GitLab, here's a list of recommended next steps.
-
-Already a pro? Just edit this README.md and make it your own. Want to make it easy? [Use the template at the bottom](#editing-this-readme)!
-
-## Add your files
-
-- [ ] [Create](https://docs.gitlab.com/ee/user/project/repository/web_editor.html#create-a-file) or [upload](https://docs.gitlab.com/ee/user/project/repository/web_editor.html#upload-a-file) files
-- [ ] [Add files using the command line](https://docs.gitlab.com/ee/gitlab-basics/add-file.html#add-a-file-using-the-command-line) or push an existing Git repository with the following command:
-
-```
-cd existing_repo
-git remote add origin https://gitlab.com/opensourcelab/openlab-site/lab-automation.git
-git branch -M main
-git push -uf origin main
-```
-
-## Integrate with your tools
-
-- [ ] [Set up project integrations](https://gitlab.com/opensourcelab/openlab-site/lab-automation/-/settings/integrations)
-
-## Collaborate with your team
-
-- [ ] [Invite team members and collaborators](https://docs.gitlab.com/ee/user/project/members/)
-- [ ] [Create a new merge request](https://docs.gitlab.com/ee/user/project/merge_requests/creating_merge_requests.html)
-- [ ] [Automatically close issues from merge requests](https://docs.gitlab.com/ee/user/project/issues/managing_issues.html#closing-issues-automatically)
-- [ ] [Enable merge request approvals](https://docs.gitlab.com/ee/user/project/merge_requests/approvals/)
-- [ ] [Automatically merge when pipeline succeeds](https://docs.gitlab.com/ee/user/project/merge_requests/merge_when_pipeline_succeeds.html)
-
-## Test and Deploy
-
-Use the built-in continuous integration in GitLab.
-
-- [ ] [Get started with GitLab CI/CD](https://docs.gitlab.com/ee/ci/quick_start/index.html)
-- [ ] [Analyze your code for known vulnerabilities with Static Application Security Testing(SAST)](https://docs.gitlab.com/ee/user/application_security/sast/)
-- [ ] [Deploy to Kubernetes, Amazon EC2, or Amazon ECS using Auto Deploy](https://docs.gitlab.com/ee/topics/autodevops/requirements.html)
-- [ ] [Use pull-based deployments for improved Kubernetes management](https://docs.gitlab.com/ee/user/clusters/agent/)
-- [ ] [Set up protected environments](https://docs.gitlab.com/ee/ci/environments/protected_environments.html)
-
-***
-
-# Editing this README
-
-When you're ready to make this README your own, just edit this file and use the handy template below (or feel free to structure it however you want - this is just a starting point!). Thank you to [makeareadme.com](https://www.makeareadme.com/) for this template.
-
-## Suggestions for a good README
-Every project is different, so consider which of these sections apply to yours. The sections used in the template are suggestions for most open source projects. Also keep in mind that while a README can be too long and detailed, too long is better than too short. If you think your README is too long, consider utilizing another form of documentation rather than cutting out information.
-
 ## Name
 Choose a self-explaining name for your project.
 
@@ -67,6 +18,21 @@ Depending on what you are making, it can be a good idea to include screenshots o
 Within a particular ecosystem, there may be a common way of installing things, such as using Yarn, NuGet, or Homebrew. However, consider the possibility that whoever is reading your README is a novice and would like more guidance. Listing specific steps helps remove ambiguity and gets people to using your project as quickly as possible. If it only runs in a specific context like a particular programming language version or operating system or has dependencies that have to be installed manually, also add a Requirements subsection.
 
 ## Usage
+1. install and start the orchestrator
+2. install the processes
+3. install pythonLab
+4. load a process
+5. install the scheduler
+6. start the scheduler
+7. simulate the process
+8. install the server manager
+9. install the device servers
+10. start the device servers in simulation mode
+11. start the visualization (optional)
+12. remove the process and load it again
+13. start running the process
+14. enjoy the simulation
+
 Use examples liberally, and show the expected output if you can. It's helpful to have inline the smallest example of usage that you can demonstrate, while providing links to more sophisticated examples if they are too long to reasonably include in the README.
 
 ## Support
---
+++ full_install_script.py
@@ -0,0 +1,233 @@
+
+import sys
+import os
+from pathlib import Path
+from typing import NamedTuple, Dict
+import pip
+import subprocess
+
+from distutils.util import strtobool
+
+# checking python version
+major_version = sys.version_info.major
+minor_version = sys.version_info.minor
+
+# this works only with python >= 3.8
+print(f"You are running python {major_version}.{minor_version}")
+
+HOME_DIR = str(Path.home())
+
+REPO_DIR = os.path.dirname(os.path.realpath(__file__))  # current directory of this repository
+
+
+def printWelcomeMessage():
+    installer_welcome_txt = (
+        "______________________________________________________\n\n"
+         " ___  ____  __      __    ___  \n"
+         "/ __)(_  _)(  )    /__\  (__ \ \n"
+         "\__ \ _)(_  )(__  /(__)\  / _/ \n"
+         "(___/(____)(____)(__)(__)(____)\n\n"
+         #"This is the SiLA2 Python3 installer\n"
+         #"It will guide you through the complete installation \n"
+         #"of the core parts of SiLA2-python ...\n"
+         #"[type: ?    - for further information\n"
+         #"       help - to list all input options    ]\n"
+         "This script will guide you through the installation of the laborchestrator\n"
+         "and the matching software-packages designed to work with it.\n"
+         "______________________________________________________\n"
+    )
+    print(installer_welcome_txt)
+
+
+class GitRepo(NamedTuple):
+    url: str
+    branch: str
+
+    def install(self, name):
+        print(f"Installing {name} from {self.url}/{self.branch}")
+        call(f"git clone {self.url} {name}")
+        os.chdir(f"./{name}")
+        call(f"git checkout {self.branch}")
+        call("git pull")
+        call("pip install -e .")
+        os.chdir("./..")
+
+orchestrator_git = GitRepo(
+    "https://gitlab.com/opensourcelab/laborchestrator.git",
+    "feature/release_V0_1_draft"
+)
+scheduler_git = GitRepo(
+    "https://gitlab.com/opensourcelab/pythonlabscheduler.git",
+    "feature/release_V0_1_draft"
+)
+tools_git = GitRepo(
+    "https://gitlab.com/StefanMa/lara-tools.git",
+    "main"
+)
+database_git = GitRepo(
+    "https://gitlab.com/StefanMa/platform_status_db.git",
+    "main"
+)
+lara_processes_git = GitRepo(
+    "https://gitlab.com/lara-uni-greifswald/lara-processes.git",
+    "feature/worker_implementation"
+)
+device_gits: Dict[str, GitRepo] = dict(
+    cytomat=GitRepo("https://gitlab.com/opensourcelab/devices/incubators_shakers/thermo_cytomat2.git",
+                          "feature/sila2_server"),
+    barcode_reader=GitRepo("https://gitlab.com/opensourcelab/devices/barcodereader/omron-laserscanner-ms-3.git",
+                          "feature/sila2_server"),
+    silafied_human=GitRpo("https://gitlab.com/StefanMa/silafiedhuman.git",
+                          "main"),
+)
+
+# --------------- installation helper functions, please do not modify -----------------------------
+def query_yes_no(question, default_answer="yes", help=""):
+    """Ask user at stdin a yes or no question
+
+    :param question: question text to user
+    :param default_answer: should be "yes" or "no"
+    :param help: help text string
+    :return:  :type: bool
+    """
+    if default_answer == "yes":
+        prompt_txt = "{question} [Y/n] ".format(question=question)
+    elif default_answer == "no":  # explicit no
+        prompt_txt = "{question} [y/N] ".format(question=question)
+    else:
+        raise ValueError("default_answer must be 'yes' or 'no'!")
+
+    while True:
+        try:
+            answer = input(prompt_txt)
+            if answer:
+                if answer == "?":
+                    print(help)
+                    continue
+                else:
+                    return strtobool(answer)
+            else:
+                return strtobool(default_answer)
+        except ValueError:
+            sys.stderr.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
+        except KeyboardInterrupt:
+            sys.stderr.write("Query interrupted by user, exiting now ...")
+            exit(0)
+
+def query(question, default_answer="", help=""):
+    """Ask user a question
+
+    :param question: question text to user
+    :param default_answer: any default answering text string
+    :param help:  help text string
+    :return: stripped answer string
+    """
+    prompt_txt = "{question} [{default_answer}] ".format(question=question, default_answer=default_answer)
+
+    while True:
+        answer = input(prompt_txt).strip()
+
+        if answer:
+            if answer == "?":
+                print(help)
+                continue
+            else:
+                return answer
+        else:
+            return default_answer
+
+def call(command=""):
+    ''' Convenient command call: it splits the command string into tokens (default separator: space)
+
+    :param command: the command to be executed by the system
+    '''
+    try:
+        cmd_lst = command.split()
+        subprocess.run(cmd_lst, check=True)
+    except subprocess.CalledProcessError as err:
+        sys.stderr.write('CalledProcessERROR:{}'.format(err))
+
+
+def run(command="", parameters=[]):
+    '''This version is closer to the subprocess version
+
+    :param command: the command to be executed by the system
+    :param parameters: parameters of this command
+    '''
+    try:
+        subprocess.run([command] + parameters, check=True, shell=True)
+    except subprocess.CalledProcessError as err:
+        sys.stderr.write('ERROR:', err)
+
+
+def runSetup(src_dir="", lib_dir=""):
+    """running a setup.py file within a pyhton script
+       it requires a lot of things set...
+       :param src_dir: directory containing the setup.py file
+       :param lib_dir: directory containing the target lib directory
+    """
+    # all path settings seem to be required by run_setup and setup.py
+    os.environ["PYTHONPATH"] = os.path.join(lib_dir, 'lib', 'python3.8', 'site-packages')
+    sys.path.append(lib_dir)
+    os.chdir(src_dir)
+    setup_file = os.path.join(src_dir, 'setup.py')
+    # this no longer works:
+    #~ run_setup(setup_file,  script_args=['install', '--prefix', lib_dir ])
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '.'])
+
+
+def make_dir_structure():
+    print("Creating directory structure")
+    call("mkdir devices")
+
+
+def install_orchestrator():
+    print("Installing laborchestrator...")
+    orchestrator_git.install('orchestrator')
+
+def install_devices():
+    print("Installing sila2 servers for devices")
+    os.chdir("./devices")
+    for name, repo in device_gits.items():
+        if query_yes_no(f"Install {name}?"):
+            repo.install(name)
+    os.chdir("./..")
+
+
+def install_tools():
+    print("Installing lara-tools...")
+    name= "lara_server_tools"
+    call(f"git clone {tools_git.url} {name}")
+    os.chdir(f"./{name}/lara_simulation")
+    call("pip install -e .")
+    os.chdir(f"../utility")
+    call("pip install -e .")
+    os.chdir("./../..")
+
+
+def run_tests():
+    print("Testing installation")
+
+def installOnLinux():
+    orchestrator_git.install('orchestrator')
+    make_dir_structure()
+    #if query_yes_no("Install scheduler?"):
+    scheduler_git.install('scheduler')
+    #if query_yes_no("Install lara-tools (needed to run&visualize simulated devices)?"):
+    install_tools()
+    #if query_yes_no("Install device servers?"):
+    install_devices()
+
+    #database_git.install("platform_status_database")
+
+    #if query_yes_no("Run pytest on every installed package? This might take about 1 or 2 minutes."):
+    run_tests()
+
+
+if __name__ == '__main__':
+    printWelcomeMessage()
+    installOnLinux()
+
+    print("Enjoy!")
+
