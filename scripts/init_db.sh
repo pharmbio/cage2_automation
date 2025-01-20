150ff780b2d3f2e01013510f81b32628ecfa5efa
StefanMa <stefanmaak@freenet.de>
2025-01-20 19:15:57 +0100

    added install scripts

---
+++ scripts/init_db.sh
@@ -0,0 +1,8 @@
+#! bin/bash
+
+cd source
+cd platform_status_db/platform_status_db
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createsuperuser
+cd ../../scripts
---
+++ scripts/install_all.sh
@@ -0,0 +1,75 @@
+#! bin/bash
+# create directory if necessary
+dir = "source"
+if [ ! -d "$dir" ]; then
+  echo "Directory '$dir' does not exist. Creating it..."
+  mkdir -p "$dir"
+fi
+cd $dir
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+if [ ! -d "laborchestrator" ]; then
+  git clone git@gitlab.com:opensourcelab/laborchestrator.git  # develop
+fi
+cd laborchestrator
+git checkout develop
+uv pip install -e .
+cd ..
+if [! -d "pythonlabscheduler"]; then
+  git clone git@gitlab.com:opensourcelab/pythonlabscheduler.git  # develop
+fi
+cd pythonlabscheduler
+git checkout develop
+uv pip install -e .
+uv pip install --upgrade protobuf  # needed for scheduler
+cd ..
+if [! -d "pythonLab"]; then
+  git clone git@gitlab.com:opensourcelab/pythonLab.git  # feature/labels
+fi
+cd pythonLab
+git checkout feature/labels
+uv pip install -e .
+cd ..
+if [! -d "platform_status_db"]; then
+  git clone https://gitlab.com/StefanMa/platform_status_db.git  # develop
+fi
+cd platform_status_db
+git checkout develop
+uv pip install -e .
+cd ..
+
+cd
+# install device connectors
+if [ ! -d "devices" ]; then
+  mkdir -p "devices"
+fi
+cd devices
+
+pairs=(
+  "washer git@gitlab.com:uppsala_automation/sila-servers/cell-washer.git"
+  "squid git@gitlab.com:uppsala_automation/sila-servers/squid.git"
+  "sealer git@gitlab.com:uppsala_automation/sila-servers/cealer.git"
+  #"cytomat https://github.com/user/repo3.git"
+  "robo_arm" "git@gitlab.com:StefanMa/genericroboticarm.git"
+)
+
+# Iterate over each pair (device, repo)
+for pair in "${pairs[@]}"; do
+  # Split the pair into device and repo
+  device=$(echo $pair | cut -d' ' -f1)
+  repo=$(echo $pair | cut -d' ' -f2)
+
+  # Check if the device directory exists
+  if [ ! -d "$device" ]; then
+    echo "Cloning $repo into $device..."
+    git clone "$repo" "$device"
+    cd "$device"
+    uv pip install -e .
+    cd ..
+  else
+    echo "$device already exists, skipping..."
+  fi
+done
+
+cd ..
