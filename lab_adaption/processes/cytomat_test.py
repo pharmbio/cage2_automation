815ed4baaddbaa177390c74ec682d86df3e17c78
StefanMa <stefanmaak@freenet.de>
2025-09-25 16:08:48 +0200

    protocol fix

--- lab_adaption/processes/cytomat_test.py
+++ lab_adaption/processes/cytomat_test.py
@@ -2,21 +2,21 @@
 Duplicate this file and add/modify the missing parts to create new processes
 """
 
-from pythonlab.resource import DynamicLabwareResource as ReagentResource  # noqa: F401
 from lab_adaption.processes.basic_process import BasicProcess
 
 
 class CytomatTest(BasicProcess):
     def __init__(self):
         super().__init__(
+            priority=10,
             num_plates=2,
             process_name="CytomatTest",
         )
 
     def init_service_resources(self):
         super().init_service_resources()
-        self.containers[1].start_position(self.incubator1, 0)
-        self.containers[0].start_position(self.hotel2, 1)
+        self.containers[0].set_start_position(self.hotel2, 0)
+        self.containers[1].set_start_position(self.incubator1, 0)
 
     def process(self):
         self.containers[1].wait_cost(100)
