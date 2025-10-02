bb8e66337843ac172e8e35d809506f796e736ce4
StefanMa <stefanmaak@freenet.de>
2025-10-02 16:32:12 +0200

    added wrapper example for wrapping observable commands

---
+++ lab_adaption/device_wrappers/custom_wrapper1.py
@@ -0,0 +1,23 @@
+import logging
+
+from sila2.client import ClientObservableCommandInstance
+from laborchestrator.structures import ProcessStep, ContainerInfo
+
+try:
+    from my_sila_server_package import Client as CustomClient
+except ModuleNotFoundError:
+    logging.warning("The sila server seems to not be installed")
+    from sila2.client import SilaClient as CustomClient
+from . import DeviceInterface
+
+
+class CustomWrapper(DeviceInterface):
+    @staticmethod
+    def get_SiLA_handler(
+        step: ProcessStep, cont: ContainerInfo, sila_client: CustomClient, **kwargs
+    ) -> ClientObservableCommandInstance:
+        my_custom_argument = step.data.get("my_key", None)
+        if not my_custom_argument:
+            logging.error("necessary argument my_key not given")
+        observable_cmd = sila_client.MySilaFeature.MyObservableCommand()
+        return observable_cmd
