0b46b9c1c9db931448beed6fcbac296417e5383a
StefanMa <stefanmaak@freenet.de>
2026-01-12 14:52:36 +0100

    edited some processes

--- lab_adaption/processes/multiflow_test.py
+++ lab_adaption/processes/multiflow_test.py
@@ -4,7 +4,7 @@ Duplicate this file and add/modify the missing parts to create new processes
 
 from pythonlab.resource import DynamicLabwareResource as ReagentResource  # noqa: F401
 from lab_adaption.processes.basic_process import BasicProcess
-from lhc_python.steps import DispenseStep
+from lhc_python.steps import DispenseStep, PrimeStep
 
 
 class MultiflowTest(BasicProcess):
@@ -20,11 +20,10 @@ class MultiflowTest(BasicProcess):
         self.containers[0].set_start_position(self.hotel2, 0)
 
     def process(self):
-        steps = [
-            DispenseStep()
-        ]
+        step = PrimeStep()
+        step.step_data = "DV103|4|1|50|2|333|-4|-4|True|50|2|7|111111111111111111111111111111111111111111111111|1|1111"
         cont = self.containers[0]
-        self.robot_arm.move(cont, self.dispenser, lidded=False)
-        self.dispenser.execute_custom_steps(labware=cont, steps=steps)
-        self.robot_arm.move(cont, self.hotel2)
+        #self.robot_arm.move(cont, self.dispenser, lidded=False)
+        self.dispenser.execute_custom_steps(labware=cont, steps=[step])
+        #self.robot_arm.move(cont, self.hotel2)
 
