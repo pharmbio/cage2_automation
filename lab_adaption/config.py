b927ddb1fc82aa518774117fa921b793a6083670
StefanMa <stefanmaak@freenet.de>
2026-05-07 15:32:17 +0200

    added test whether config.py is sucessfully read

--- lab_adaption/config.py
+++ lab_adaption/config.py
@@ -32,3 +32,8 @@ default_scheduling_time = 1  # seconds
 scheduling_algorithm = "BottleneckPD"
 
 process_module = processes
+
+# configures the initial view of the GUI.
+arm_open = True  # View for genericroboticarm
+db_open = True  # View for present labware in database
+browser_open = True  # Sila-browser
