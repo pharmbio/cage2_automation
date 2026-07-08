8644ab0a2100a009165f082d8c5c36b65ae3862b
StefanMa <stefanmaak@freenet.de>
2026-07-08 19:30:32 +0200

    extended multiflow test

--- lab_adaption/processes/multiflow_test.py
+++ lab_adaption/processes/multiflow_test.py
@@ -35,6 +35,6 @@ class MultiflowTest(BasicProcess):
         peri_dispense_step = DemoStep(name="peri_dispense_demo", step_def="DV103|1|10|High|0|333|0|0|True|10|2|111111111111111111111111111111111111111111111111|1111|1", body="")
         cont = self.containers[0]
         #self.robot_arm.move(cont, self.dispenser, lidded=False)
-        self.dispenser.execute_custom_steps(labware=cont, steps=[prime_step])
+        self.dispenser.execute_custom_steps(labware=cont, steps=[prime_step, peri_dispense_step])
         #self.dispenser.run_protocol(labware=cont, protocol=str(protocol_path))
         #self.robot_arm.move(cont, self.hotel2)
