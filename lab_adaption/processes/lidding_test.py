eb43c9f6027628897238c96f1e7c11150d42df8c
StefanMa <stefanmaak@freenet.de>
2026-09-15 14:40:04 +0200

    created usefull test for testing efficient lidding

--- lab_adaption/processes/lidding_test.py
+++ lab_adaption/processes/lidding_test.py
@@ -1,5 +1,6 @@
 """
-Duplicate this file and add/modify the missing parts to create new processes
+Tests the lidding: a lidded plate is unlidded on its way into Cytomat1, rests there for 30 seconds
+and gets its lid back on when it returns to Hotel2.
 """
 
 from lab_adaption.processes.basic_process import BasicProcess
@@ -19,5 +20,9 @@ class LiddingTest(BasicProcess):
             cont.set_start_position(self.hotel2, i)
 
     def process(self):
-        self.robot_arm.move(self.containers[0], self.hotel1, lidded=False)
+        # the lid is parked in the lid storage while the plate is inside the cytomat
+        self.robot_arm.move(self.containers[0], self.incubator1, lidded=False)
+        # no temperature control, the plate simply rests inside
+        self.incubator1.incubate(self.containers[0], duration=30, temperature=None)
+        # the lid is picked up from its parking position again
         self.robot_arm.move(self.containers[0], self.hotel2, lidded=True)
